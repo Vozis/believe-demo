eb9430ec7d69b04083bb9bce9f3864fc328d2a44
Vozis <sizov.ilya1996@gmail.com>
2025-05-22 20:20:28 +0300

    fix

--- src/components/Believe/Believe.tsx
+++ src/components/Believe/Believe.tsx
@@ -1,19 +1,17 @@
-import { useState, useEffect } from 'react';
 import styles from './Believe.module.scss';
-import { AnimatePresence, motion } from 'motion/react';
 import AnimatedSection from '../AnimatedSection/AnimatedSection.tsx';
 
 const Believe = () => {
-    const [showGradient, setShowGradient] = useState(false);
-
-    useEffect(() => {
-        // Задержка для появления градиента после завершения анимации
-        const timer = setTimeout(() => {
-            setShowGradient(true);
-        }, 1000); // 1 секунда задержки, можно настроить по необходимости
-
-        return () => clearTimeout(timer);
-    }, []);
+    // const [showGradient, setShowGradient] = useState(false);
+    //
+    // useEffect(() => {
+    //     // Задержка для появления градиента после завершения анимации
+    //     const timer = setTimeout(() => {
+    //         setShowGradient(true);
+    //     }, 1000); // 1 секунда задержки, можно настроить по необходимости
+    //
+    //     return () => clearTimeout(timer);
+    // }, []);
 
     return (
         <div className={`${styles.bel} df jcc`}>
@@ -24,17 +22,17 @@ const Believe = () => {
                     </p>
                     <span>don’t you?</span>
                 </AnimatedSection>
-                <AnimatePresence>
-                    {showGradient && (
-                        <motion.div
-                            className={styles.gradient}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 0.6 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 1.2, ease: 'easeInOut' }}
-                        />
-                    )}
-                </AnimatePresence>
+                {/*<AnimatePresence>*/}
+                {/*    {showGradient && (*/}
+                {/*        <motion.div*/}
+                {/*            className={styles.gradient}*/}
+                {/*            initial={{ opacity: 0 }}*/}
+                {/*            animate={{ opacity: 0.6 }}*/}
+                {/*            exit={{ opacity: 0 }}*/}
+                {/*            transition={{ duration: 1.2, ease: 'easeInOut' }}*/}
+                {/*        />*/}
+                {/*    )}*/}
+                {/*</AnimatePresence>*/}
                 <div className='df jcc'>
                     <button>Believe</button>
                 </div>
